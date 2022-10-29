4225efebcfcc722d8c92ce4db4e53453b1317195
Zuhair Haroon Khan <zukh@easj.dk>
2022-10-29 21:05:03 +0200

    Add staged commit

--- MinimalAPISampleProject/Data/AppDbContext.cs
+++ MinimalAPISampleProject/Data/AppDbContext.cs
@@ -5,6 +5,7 @@ namespace MinimalAPISampleProject.Data
 {
     public class AppDbContext : DbContext
     {
+
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)   
         {
 
